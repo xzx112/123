1af2395b05e3a9f7f29a562f7b4e2f2c02294324
x112 <xxzzxx55@sina.com>
2016-09-23 09:56:27 +0800

    111

--- 22.php
+++ 22.php
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php 
 $aa = phpinfo();
 12312312
 
 /* ewrerthyhj */
-=======
-2222
->>>>>>> parent of c18a0c0... php
-=======
-2222<?php 
-$aa = phpinfo();
->>>>>>> parent of 2bd8dbd... 123
+
